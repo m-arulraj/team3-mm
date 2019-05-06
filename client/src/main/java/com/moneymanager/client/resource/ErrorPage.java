af8081dc971acb6584a81d4db785da38b26d67ac
AjithKumarKS <ajithkumarseethapathi@gmail.com>
2019-05-06 21:54:33 +0530

    added Year in income-vs-investment-vs-expenses-report.jsp

--- client/src/main/java/com/moneymanager/client/resource/ErrorPage.java
+++ client/src/main/java/com/moneymanager/client/resource/ErrorPage.java
@@ -5,17 +5,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorPage  {
+public class ErrorPage implements ErrorController {
 
-	/*
-	 * private static final String PATH = "/error";
-	 * 
-	 * @RequestMapping(value = PATH) public String error() { return
-	 * "error-page.jsp"; }
-	 */
-	
-	
-	
-	
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return PATH;
+	}
+
+	private static final String PATH = "/error";
+
+	@RequestMapping(value = PATH)
+	public String error() {
+		return "error-page";
+	}
 
 }
