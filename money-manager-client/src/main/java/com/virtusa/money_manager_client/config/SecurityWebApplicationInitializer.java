613c75dd362513600e58149811deb0438892a5c0
AjithKumarKS <ajithkumarseethapathi@gmail.com>
2019-05-09 18:58:02 +0530

    removed unwanted application money-manager-client

--- money-manager-client/src/main/java/com/virtusa/money_manager_client/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.virtusa.money_manager_client.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
